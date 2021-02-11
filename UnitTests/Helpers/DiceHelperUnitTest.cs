1bfe3aea414804bc19d1a73d0f13c7044759bf4f
Dang Nguyen <dnguyen1@seattleu.edu>
2021-02-10 23:43:30 -0800

    Added RollDice Invalid Roll Zero Dice to DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -35,5 +35,36 @@ namespace UnitTests.Helpers
             Assert.AreEqual(true, result >= 1);
             Assert.AreEqual(true, result <= 6);
         }
+
+        [Test]
+        public void RollDice_Invalid_Roll_Forced_1_Should_Return_1()
+        {
+            // Arrange
+            DiceHelper.ForceRollsToNotRandom = true;
+            DiceHelper.ForcedRandomValue = 1;
+
+            // Act
+            var result = DiceHelper.RollDice(1, 1);
+
+            // Reset
+            DiceHelper.ForceRollsToNotRandom = false;
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void RollDice_Invalid_Roll_Zero_Dice_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(1, 0);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
     }
 }
